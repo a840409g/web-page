f9e034d95d932990a20a72536f015565c370a525
joe0409_chen <joe0409_chen@mail.chungyo.net>
2022-07-30 15:09:24 +0800

    feat: 新聞頁面左側導覽列表

--- src/views/NewsHome/components/NewsHomeSideBar/index.jsx
+++ src/views/NewsHome/components/NewsHomeSideBar/index.jsx
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect }from 'react';
+import './NewsHomeSideBar.css';
+import HomeIcon from '../../../../static/images/home.png';
+import navBarList from './navBarList';
 
 const NewsHomeSideBar = () => {
+    const [ curNavStatus, setCurNavStatus ] = useState('');
+    const newsNavLiClass = id => {
+        return    curNavStatus === id
+            ? 'news-nav-li nav-info-selected'
+            : 'news-nav-li';
+    };
+    const navBarLi = navBarList.map(({title, id}) => (
+        <li
+            className = { newsNavLiClass(id) }
+            key = { id }
+            onClick = {
+                () => {
+                    setCurNavStatus(id);
+                }
+            }
+        >
+            <img
+                src = { HomeIcon }
+                className = "news-nav-icon"
+            />
+            <p className = "news-nav-text">
+                { title }
+            </p>
+        </li>
+    ));
+
+    useEffect(() => {
+        setCurNavStatus(navBarList[0].id);
+    }, []);
+
     return (
         <div id = "news-home-sideBar-root">
-            NewsHomeSideBar
+            { navBarLi }
         </div>
     );
 };
