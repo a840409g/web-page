0c6213eee9e91dba39e85dd0140c5f0c91ba1a63
joe0409_chen <joe0409_chen@mail.chungyo.net>
2022-07-30 14:44:34 +0800

    feat: 新聞頁面footer

--- src/views/NewsHome/components/NewsHomeFooter/index.jsx
+++ src/views/NewsHome/components/NewsHomeFooter/index.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import './NewsHomeFooter.css';
 
 const NewsHomeFooter = () => {
+    const footerInfoArr = [
+        '隱私權和Cookie',
+        '法律聲明',
+        '廣告',
+        '關於我們的廣告',
+        '說明',
+        '意見反饋'
+    ];
+    const footerInfoLi = footerInfoArr.map(li => (
+        <li
+            className = "footer-info-li"
+            key = { li }
+        >
+            {li}
+        </li>
+    ));
+
+
     return (
         <div id = "news-home-footer-root">
-            NewsHomeFooter
+            <ul className = "footer-info-wrap">
+                {footerInfoLi }
+            </ul>
+            <p className = "copy-right-text">
+                @2021 Microsoft
+            </p>
         </div>
     );
 };
