2b3ed73c5e96209b6494f8f8ccf76c6fe4a70bdd
joe0409_chen <joe0409_chen@mail.chungyo.net>
2022-07-30 14:37:51 +0800

    feat: 新聞頁面header

--- src/views/NewsHome/components/NewsHomeHeader/index.jsx
+++ src/views/NewsHome/components/NewsHomeHeader/index.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import './NewsHomeHeader.css';
+import Logo from '../../../../static/images/Bing-logo.png';
+import Search from '../../../../static/images/search.png';
 
 const NewsHomeHeader = () => {
     return (
         <div id = "news-home-header-root">
-            NewsHomeHeader
+            <img
+                src = { Logo }
+                className = "news-logo"
+            />
+            <div className = "news-search-input-bar">
+                <input
+                    type = "text"
+                    placeholder = "熱門搜尋"
+                    className = "news-search-input"
+                />
+                <img
+                    src = { Search }
+                    className = "search-icon"
+                />
+            </div>
         </div>
     );
 };
